e64448f55f51b8e7293fa18dfbdffb9e78d27d16
mrxise <103466470+mrxise@users.noreply.github.com>
2023-01-22 23:53:19 +0100

    Added the plate slicer

--- main.py
+++ main.py
@@ -27,6 +27,33 @@ for i, item in enumerate(theTable):
         nums.append(int(raw))
 wilayas = list(zip(nums, names))
 wilayas.sort(key=lambda x: x[0])
-s = tabulate(wilayas)
 
-print(s)
+
+matricule = "1234510140"
+# for each in matricule:
+
+
+def slicer():
+    while True:
+        try:
+            matricule = input("Enter your matricule")
+
+            if not isinstance(matricule, str) or not re.fullmatch(r'\d{10}', matricule):
+                raise ValueError("Invalide license plate")
+            ser = matricule[:5]
+            ann = matricule[5:8]
+            wil = matricule[8:]
+            return int(ser), int(ann), int(wil)
+
+        except ValueError as e:
+            print(e)
+            continue
+        else:
+            break
+
+
+# 111 = Private 2011
+#  199 = Private 1999
+serial, anne, wil = slicer()
+print(
+    f"Your vehicle's serial number is {serial} and it was made in year {anne} and you are from {wilayas[wil-1]} ")
